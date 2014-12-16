7fd083d1886ecf4a27137aa395653ec7306f08c0
Jan Horsman <jan@jhorsman.nl>
2014-12-16 23:36:34 +0200

    adding clickable login image

--- source/login-bookmarklet.js
+++ source/login-bookmarklet.js
@@ -1,2 +1,5 @@
-$(".splash .fields").wrap("<form onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); alert(\"submit\"); return false;'/>")
-$("<input type='submit' value='Submit'>").insertAfter(".login_button")
+$(".splash .fields").wrap("<form id='loginForm' onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); return false;'/>")
+$("<input id='formSubmitButton' type='submit' value='Submit'/>").appendTo(".login_button");
+
+imageButton = $("div.login_button.xref").children("img");
+$("<img src='SDL%20LiveContent%20Login_files/img_login_button_reachbase-2014061155GMT.png' onClick='$(\"#formSubmitButton\").click()'/>").insertAfter(imageButton);
