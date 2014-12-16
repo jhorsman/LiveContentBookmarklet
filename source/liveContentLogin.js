cb0b96d845bbd79b7cfeb20a55a7785f80db7441
Jan Horsman <jan@jhorsman.nl>
2014-12-16 23:44:33 +0200

    fix: do not add the from element and the button if it was already set

--- source/liveContentLogin.js
+++ source/liveContentLogin.js
@@ -1,9 +1,11 @@
 if(window.location.href.contains("docs.sdl.com") && document.getElementById("username")) {
-    $(".splash .fields").wrap("<form id='loginForm' onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); return false;'/>");
-    $("<input id='formSubmitButton' type='submit' value='Submit' style='display: none;'/>").appendTo(".login_button");
-    imageButton = $("div.login_button.xref").children("img");
-    imageButton.hide();
-    $("<img onClick='$(\"#formSubmitButton\").click()'/>").attr("src", imageButton.attr("src")).insertAfter(imageButton);
+    if($("#loginForm").length == 0) {
+        $(".splash .fields").wrap("<form id='loginForm' onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); return false;'/>");
+        $("<input id='formSubmitButton' type='submit' value='Submit' style='display: none;'/>").appendTo(".login_button");
+        imageButton = $("div.login_button.xref").children("img");
+        imageButton.hide();
+        $("<img onClick='$(\"#formSubmitButton\").click()'/>").attr("src", imageButton.attr("src")).insertAfter(imageButton);
+    }
 } else {
     window.location.href="http://docs.sdl.com/LiveContent/"
 }
