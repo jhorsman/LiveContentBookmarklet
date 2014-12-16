20d19235a3035719a4e04dd7c46ff1d5730c58c9
Jan Horsman <jan@jhorsman.nl>
2014-12-16 23:37:22 +0200

    hide unused buttons

--- source/login-bookmarklet.js
+++ source/login-bookmarklet.js
@@ -1,5 +1,6 @@
 $(".splash .fields").wrap("<form id='loginForm' onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); return false;'/>")
-$("<input id='formSubmitButton' type='submit' value='Submit'/>").appendTo(".login_button");
+$("<input id='formSubmitButton' type='submit' value='Submit' style='display: none;'/>").appendTo(".login_button");
 
 imageButton = $("div.login_button.xref").children("img");
+imageButton.hide();
 $("<img src='SDL%20LiveContent%20Login_files/img_login_button_reachbase-2014061155GMT.png' onClick='$(\"#formSubmitButton\").click()'/>").insertAfter(imageButton);
