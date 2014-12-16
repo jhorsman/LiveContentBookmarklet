b1b68755b56edb263a3f3ee0982190e4a8916e59
Jan Horsman <jan@jhorsman.nl>
2014-12-16 23:53:14 +0200

    Added "Enhanced by LiveContent bookmarklet" message

--- source/liveContentLogin.js
+++ source/liveContentLogin.js
@@ -1,5 +1,7 @@
 if(window.location.href.indexOf("docs.sdl.com") != -1 && document.getElementById("username")) {
     if($("#loginForm").length == 0) {
+        $(".fields").css("position", "relative");
+        $("<p style='color: orange; position: absolute; right: 40px; top: -8px;'>Enhanced by LiveContent bookmarklet</p>").prependTo(".fields");
         $(".splash .fields").wrap("<form id='loginForm' onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); return false;'/>");
         $("<input id='formSubmitButton' type='submit' value='Submit' style='display: none;'/>").appendTo(".login_button");
         imageButton = $("div.login_button.xref").children("img");
