4360177837ee98894c29251585e80383647560c8
Jan Horsman <jan@jhorsman.nl>
2014-12-16 23:43:23 +0200

    Redirect user to LiveContent when the bookmarklet is run on another site.

--- source/liveContentLogin.js
+++ source/liveContentLogin.js
@@ -1,5 +1,9 @@
-$(".splash .fields").wrap("<form id='loginForm' onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); return false;'/>");
-$("<input id='formSubmitButton' type='submit' value='Submit' style='display: none;'/>").appendTo(".login_button");
-imageButton = $("div.login_button.xref").children("img");
-imageButton.hide();
-$("<img onClick='$(\"#formSubmitButton\").click()'/>").attr("src", imageButton.attr("src")).insertAfter(imageButton);
+if(window.location.href.contains("docs.sdl.com") && document.getElementById("username")) {
+    $(".splash .fields").wrap("<form id='loginForm' onsubmit='javascript: $(\"div.login_button.xref\").children(\"img\").click(); return false;'/>");
+    $("<input id='formSubmitButton' type='submit' value='Submit' style='display: none;'/>").appendTo(".login_button");
+    imageButton = $("div.login_button.xref").children("img");
+    imageButton.hide();
+    $("<img onClick='$(\"#formSubmitButton\").click()'/>").attr("src", imageButton.attr("src")).insertAfter(imageButton);
+} else {
+    window.location.href="http://docs.sdl.com/LiveContent/"
+}
